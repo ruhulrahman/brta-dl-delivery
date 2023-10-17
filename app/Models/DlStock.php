b49886dba83ffd09ef3d9356b7f61d5c382f0f60
ruhulrahman2233 <ruhulrahman2233@gmail.com>
2023-10-17 14:35:18 +0600

    updated

--- app/Models/DlStock.php
+++ app/Models/DlStock.php
@@ -14,19 +14,30 @@ class DlStock extends Model{
     // protected $primaryKey = NULL; // or null
     protected $primaryKey = 'id';
 
+    // protected $guarded = ['id'];
+
     public $incrementing = false;
 
     protected $fillable = [
         'id',
         'reference_number',
         'serial_number',
+        'dl_number',
+        'name',
+        'father_name',
+        'dob',
+        'blood',
         'entry_box_number',
         'receiving_box_number',
         'receive_date',
         'delivery_date',
         'comment',
+        'delivered_id',
         'creator_id',
+        'editor_id',
         'created_at',
+        'updated_at',
+        'deleted_at',
     ];
 
 	// protected $guarded=['id'];
